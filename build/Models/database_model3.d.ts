fdacbd663c098ef5b4200906b8d739c76b9625de
BomberRoyale <121585000+BomberRoyale@users.noreply.github.com>
2023-01-12 19:43:42 -0400

    Delete build directory

--- build/Models/database_model3.d.ts
+++
@@ -1,5 +0,0 @@
-export default class Database_model3 {
-    connect(): void;
-    loadUser(user: string, senha: string, callback?: any): void;
-    cadastrarUsuario(user: string, senha: string, nascimento: string, callback?: any): void;
-}
